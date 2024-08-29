27707cd0256e1d335f68496a914fb5008770dd45
Anderson-Andre-P <eleu.ander@gmail.com>
2024-08-29 17:04:59 -0300

    refactor: colors of app

--- lib/common/constants/app_colors.dart
+++ lib/common/constants/app_colors.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 class AppColors {
   AppColors._();
 
+  static const Color primary = Color(0xFF0178F1);
+  static const Color secondary = Color(0xFF0B2161);
   static const Color blueLightOne = Color(0xFF0178F1);
   static const Color blueLightTwo = Color(0xFF0B2161);
 
@@ -18,6 +20,7 @@ class AppColors {
 
   static const Color whiteLight = Colors.white;
   static const Color iceLight = Color.fromARGB(255, 242, 245, 248);
+  static const Color scaffold = Color.fromARGB(255, 242, 245, 248);
 
   static const Color greyLight = Color(0xFF444444);
 }
