37a3d2e076b9199fb70a6478231a14b1f4f001f7
Anderson-Andre-P <eleu.ander@gmail.com>
2024-08-29 14:26:39 -0300

    feat: add custom text button

--- lib/features/onboarding/onboarding_page.dart
+++ lib/features/onboarding/onboarding_page.dart
@@ -5,6 +5,7 @@ import 'package:finance/common/constants/app_text_style.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
+import '../../common/widgets/custom_text_button.dart';
 import '../../common/widgets/primary_button.dart';
 
 class OnboardingPage extends StatelessWidget {
@@ -45,11 +46,9 @@ class OnboardingPage extends StatelessWidget {
                 },
               ),
             ),
-            Text(
-              "Already have account? Log In...",
-              style: AppTextStyle.smallText.copyWith(
-                color: AppColors.greyLight,
-              ),
+            const CustomTextSpan(
+              primaryText: 'Already have an account?',
+              actionText: 'Log In',
             ),
             32.verticalSpace,
           ],
